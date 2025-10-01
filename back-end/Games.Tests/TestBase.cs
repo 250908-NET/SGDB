5f5aa7b75a3893a21b3888b065a5feb1b7f22843
Daniel Scally <44758401+dascally@users.noreply.github.com>
2025-10-01 17:48:10 -0400

    test: Abstract DB seed method to helper class

--- back-end/Games.Tests/TestBase.cs
+++ back-end/Games.Tests/TestBase.cs
@@ -1,6 +1,5 @@
-﻿using Games.Data;
-using Games.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Games.Data;
 
 namespace Games.Tests;
 
@@ -15,72 +14,13 @@ public class TestBase : IDisposable
             .Options;
 
         Context = new GamesDbContext(options);
+        Context.Database.EnsureDeleted();
         Context.Database.EnsureCreated();
     }
 
     protected async Task SeedDataAsync()
     {
-        // Seed platforms
-        var snes = new Platform
-        {
-            PlatformId = 1,
-            Name = "Super Nintendo",
-        };
-        var ds = new Platform
-        {
-            PlatformId = 2,
-            Name = "Nintendo DS",
-        };
-
-        // Seed companies
-        var square = new Company
-        {
-            CompanyId = 1,
-            Name = "Square",
-        };
-        var gameFreak = new Company
-        {
-            CompanyId = 2,
-            Name = "Game Freak",
-        };
-
-        // Seed genres
-        var rpg = new Genre
-        {
-            GenreId = 1,
-            Name = "RPG",
-        };
-
-        // Seed games
-        var finalFantasy6 = new Game
-        {
-            GameId = 1,
-            Name = "Final Fantasy VI",
-            Developer = square,
-            ReleaseDate = new DateTime(1994, 4, 2),
-        };
-        var pokemonPlatinum = new Game
-        {
-            GameId = 2,
-            Name = "Pokémon Platinum",
-            Developer = gameFreak,
-            ReleaseDate = new DateTime(2008, 9, 13),
-        };
-
-        // Seed game-platform join table
-        var gp1 = new GamePlatform { GameId = 1, PlatformId = 1 };
-        var gp2 = new GamePlatform { GameId = 2, PlatformId = 2 };
-
-        // Seed game-genre join table
-        var gg1 = new GameGenre { GameId = 1, GenreId = 1 };
-        var gg2 = new GameGenre { GameId = 2, GenreId = 1 };
-
-        Context.Platforms.AddRange(snes, ds);
-        Context.Companies.AddRange(square, gameFreak);
-        Context.Games.AddRange(finalFantasy6, pokemonPlatinum);
-        Context.GamePlatforms.AddRange(gp1, gp2);
-
-        await Context.SaveChangesAsync();
+        await Utilities.SeedTestDbAsync(Context);
     }
 
     public void Dispose()
---
+++ back-end/Games.Tests/TestWebApplicationFactory.cs
@@ -0,0 +1,46 @@
+using System.Data.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Games.Data;
+
+namespace Games.Tests;
+
+public class TestWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(d =>
+                d.ServiceType == typeof(IDbContextOptionsConfiguration<GamesDbContext>)
+            );
+
+            services.Remove(dbContextDescriptor);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbConnection));
+
+            services.Remove(dbConnectionDescriptor);
+
+            // Create open SqliteConnection so EF won't automatically close it.
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+
+                return connection;
+            });
+
+            services.AddDbContext<GamesDbContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}
